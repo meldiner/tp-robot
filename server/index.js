ea3398776911cbc89b1421fa209cd49f5c9afc69
Ron Meldiner <ron.meldiner@autodesk.com>
2016-06-10 19:49:38 -0700

    feat(dynip): adding updating no-ip when server starts

--- server/index.js
+++ server/index.js
@@ -2,9 +2,13 @@
 
 const WebSocketServer = require('ws').Server;
 const SerialPort = require('serialport').SerialPort;
+const NoIpUpdater = require('noip-updater');
 
 const wsPort = process.env.WS_PORT;
 const serialPortPath = process.env.SERIAL_PORT_PATH;
+const noIpHostname = process.env.NO_IP_HOSTNAME;
+const noIpUser = process.env.NO_IP_USER;
+const noIpPass = process.env.NO_IP_PASS;
 
 let port = new SerialPort(serialPortPath, {baudRate: 115200});
 let ws = new WebSocketServer({port: wsPort});
@@ -39,5 +43,11 @@ ws.on('connection', client => {
     console.log("socket connection closed");
     let position = connections.indexOf(this);
     connections.splice(position, 1); // delete connection from the array
+  });
+});
+
+NoIpUpdater.getPublicIP(function(ip) {
+  NoIpUpdater.updateNoIP(noIpUser, noIpPass, noIpHostname, ip, false, function(body, response, error) {
+    console.log('response from no-ip: ' + body);
   });
 });
